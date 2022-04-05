e049b52b744569e929eb52ab34c4a93fb5f07b4c
Alejandro Sánchez <alejandro10m_568@hotmail.com>
2022-04-05 13:16:41 -0500

    Add filter by car year

--- js/app.js
+++ js/app.js
@@ -67,7 +67,7 @@ function getSelectValue(e, objectProperty){ datosBusqueda[`${objectProperty}`] =
 
 // Función que filtra en base a la búsqueda
 function filtrarAuto(){
-    const resultado = autos.filter( filtrarMarca );
+    const resultado = autos.filter( filtrarMarca ).filter( filtrarYear )
 
     console.log(resultado);
 }
@@ -76,6 +76,14 @@ function filtrarMarca(auto){
     const { marca } = datosBusqueda;
     if( marca ){ 
         return auto.marca === marca;
+    }
+    return auto;
+}
+
+function filtrarYear(auto) {
+    const { year } = datosBusqueda;
+    if( year ){ 
+        return auto.year === parseInt(year);
     }
     return auto;
 }
