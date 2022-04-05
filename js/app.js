f3edd51db52137405883fdf382def665937022f6
Alejandro Sánchez <alejandro10m_568@hotmail.com>
2022-04-05 12:23:03 -0500

    Add years in 'year select element'

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 // Variables
-const resultado = document.querySelector('#resultado');
+const resultado = document.querySelector('#resultado'),
+      yearSelected = document.querySelector('#year'),
+      maxYear = new Date().getFullYear(),
+      minYear = maxYear - 12;
 
 // Eventos
 document.addEventListener('DOMContentLoaded', ()=> {
-   mostrarAutos(); 
+   mostrarAutos(); // Muestra los autos al cargar
+
+   // Llena las opciones de años
+   llenarSelect();
 });
 
 
@@ -19,4 +25,13 @@ function mostrarAutos(){
         // Insertar en el HTML
         resultado.appendChild(autoHTML);
     });
+}
+
+function llenarSelect(){
+    for(let i = maxYear ; i >= minYear ; i--){
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion); // Agrega las opciones del año al select
+    }
 }
